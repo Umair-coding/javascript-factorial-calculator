a70dc975db1542a68f03dd15837ad5cefd6df944
code-with-umair <umairdahar944@gmail.com>
2026-07-05 11:38:52 +0500

    Add files via upload

---
+++ Factorial/index.js
@@ -0,0 +1,121 @@
+let n = Number(prompt("Enter a number betweeen 1 to 10"));
+
+if(n == 1 ){
+    let answer = 1;
+    
+    for (let i = 2; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 2){
+    let answer = 1;
+    
+    for (let i = 3; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 3){
+    let answer = 1;
+    
+    for (let i = 4; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 4){
+    let answer = 1;
+    
+    for (let i = 5; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 5){
+    let answer = 1;
+    
+    for (let i = 6; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 6){
+    let answer = 1;
+    
+    for (let i = 7; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 7){
+    let answer = 1;
+    
+    for (let i = 8; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 8){
+    let answer = 1;
+    
+    for (let i = 9; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else if(n == 9){
+    let answer = 1;
+    
+    for (let i = 10; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+
+}
+
+else{
+    let answer = 1;
+    
+    for (let i = 11; i >= 1; i--) {
+        answer = answer*i;
+        
+        
+    }
+    console.log(answer);
+}
+
